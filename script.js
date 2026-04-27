10c29c2e921be29ac1252a90a46a62984aad80d5
Gabriel <killo7425@gmail.com>
2026-04-27 07:53:15 -0300

    Continuação do Script e ajustes no css

--- script.js
+++ script.js
@@ -9,7 +9,32 @@ const cartCounter = document.getElementById("cart-count");
 const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
+// Abrir o modal do carrinho
 cartBtn.addEventListener("click", () => {
     modalCart.style.display = "flex";
 });
+// Fechar o modal do carrinho
+closeModalBtn.addEventListener("click", () => {
+    modalCart.style.display = "none";
+})
+// Fechar o modal do carrinho clicando fora do conteúdo
+modalCart.addEventListener("click", (e) => {
+    if (e.target === modalCart) {
+        modalCart.style.display = "none";
+    }
+});
 
+// Adicionar itens ao carrinho
+menu.addEventListener("click", (e) => {
+    let parentButton = e.target.closest(".add-to-cart-btn");
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+        addToCart(name, price);
+    }
+})
+
+// Função para adicionar itens ao carrinho
+function addToCart(name, price) {
+    const item = document.createElement("div")
+}
